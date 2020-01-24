bc3202b2218c62a07ca454b3db981a090af34583
d00fy <doofy2020@icloud.com>
2020-01-24 13:16:33 +0900

    デフォルトの状態

---
+++ react-three-trial/src/work.js
@@ -0,0 +1,21 @@
+/** @jsx jsx */
+import { useRef } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import { css, jsx } from "@emotion/core";
+
+const theme = css`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Thing = () => {
+  return <mesh></mesh>;
+};
+
+export const Work = () => (
+  <div css={theme}>
+    <Canvas>
+      <Thing />
+    </Canvas>
+  </div>
+);
